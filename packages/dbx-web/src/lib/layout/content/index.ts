88667b98f8aac4d84147c1c9ebcc9dd6f7b2203c
Derek Burgman <derek@dereekb.com>
2022-02-04 06:09:43 -0600

    build: build fix

--- packages/dbx-web/src/lib/layout/content/index.ts
+++ packages/dbx-web/src/lib/layout/content/index.ts
@@ -1,5 +1,7 @@
-export * from './content.layout.module';
 export * from './content.border.component';
+// export * from './content.box.anchor.component';
+export * from './content.box.component';
 export * from './content.component';
 export * from './content.container.component';
 export * from './content.elevate.component';
+export * from './content.layout.module';
