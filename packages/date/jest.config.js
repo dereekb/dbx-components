b43d911b28dd114542d73346113b5544903f7596
Derek Burgman <derek@dereekb.com>
2022-02-11 01:25:41 -0600

    test: added multiple timezone tests to date
    - Added nx target for running different timezones against the date tests.

--- packages/date/jest.config.js
+++ packages/date/jest.config.js
@@ -1,3 +1,10 @@
+// Allow our tests to also include a custom timezone.
+// Example command using this: "TZ=america/chicago nx test date"
+const timezone = process.env.TZ ?? Intl.DateTimeFormat()?.resolvedOptions()?.timeZone ?? 'utc';
+const timezoneKey = timezone.toLowerCase().replace('/', '-');
+
+process.env.JEST_JUNIT_OUTPUT_NAME = `${timezoneKey.toLowerCase()}.date.junit.xml`;
+
 module.exports = {
   displayName: 'date',
   preset: '../../jest.preset.js',
