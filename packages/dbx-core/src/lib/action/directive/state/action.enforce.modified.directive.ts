5a4c4b267f95eda02abccc0fb8a9ae6ab910f738
Derek Burgman <derek@dereekb.com>
2022-03-10 04:13:52 -0600

    feat: added dbxActionEnforceModified

---
+++ packages/dbx-core/src/lib/action/directive/state/action.enforce.modified.directive.ts
@@ -0,0 +1,44 @@
+import { Directive, Host, Input, OnInit, OnDestroy } from '@angular/core';
+import { BehaviorSubject, combineLatest, delay } from 'rxjs';
+import { AbstractSubscriptionDirective } from '../../../subscription';
+import { DbxActionContextStoreSourceInstance } from '../../action.store.source';
+
+export const APP_ACTION_ENFORCE_MODIFIED_DIRECTIVE_KEY = 'dbx_action_enforce_modified';
+
+/**
+ * Directive that toggles disabling an action if the action is not marked modified.
+ */
+@Directive({
+  selector: '[dbxActionEnforceModified]'
+})
+export class DbxActionEnforceModifiedDirective extends AbstractSubscriptionDirective implements OnInit, OnDestroy {
+
+  private _enabled = new BehaviorSubject<boolean>(true);
+
+  constructor(@Host() public readonly source: DbxActionContextStoreSourceInstance) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.sub = combineLatest([this.source.isModified$, this._enabled]).pipe(delay(0)).subscribe(([modified, enableDirective]) => {
+      const disable = enableDirective && !modified;
+      this.source.disable(APP_ACTION_ENFORCE_MODIFIED_DIRECTIVE_KEY, disable);
+    });
+  }
+
+  override ngOnDestroy(): void {
+    super.ngOnDestroy();
+    this._enabled.complete();
+    this.source.enable(APP_ACTION_ENFORCE_MODIFIED_DIRECTIVE_KEY);
+  }
+
+  @Input('[dbxActionEnforceModified]')
+  get enabled(): boolean {
+    return this._enabled.value;
+  }
+
+  set enabled(enabled: boolean) {
+    this._enabled.next(enabled ?? true);
+  }
+
+}
--- packages/dbx-core/src/lib/action/directive/state/index.ts
+++ packages/dbx-core/src/lib/action/directive/state/index.ts
@@ -5,3 +5,4 @@ export * from './action.success.component';
 export * from './action.success.directive';
 export * from './action.value.directive';
 export * from './action.working.component';
+export * from './action.enforce.modified.directive';
