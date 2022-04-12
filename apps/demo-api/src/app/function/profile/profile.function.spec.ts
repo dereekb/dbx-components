702377627c40c077ee77783b2c1613e302861272
Derek Burgman <derek@dereekb.com>
2022-04-12 03:34:53 -0500

    ci: enabled demo-api tests in circleci

--- apps/demo-api/src/app/function/profile/profile.function.spec.ts
+++ apps/demo-api/src/app/function/profile/profile.function.spec.ts
@@ -2,22 +2,44 @@ import { profileSetUsername } from './profile.set.username';
 import { SetProfileUsernameParams } from '@dereekb/demo-firebase';
 import { DemoApiFunctionContextFixture, demoApiFunctionContextFactory, demoAuthorizedUserContext } from '../../../test/fixture';
 
+/**
+ * NOTES:
+ * 
+ * These tests demonstrate testing with @dereekb/firestore-server's test components.
+ * 
+ * We can easily generate and nest contexts that go with our jest directives.
+ */
+
+// Our test requires functions, so we use a DemoApiFunctionContextFixture.
+// Every test is done within its own context; the firestore/auth/etc. is empty between each test since under the hood our test app name changes.
 demoApiFunctionContextFactory((f: DemoApiFunctionContextFixture) => {
 
+  // jest describe
   describe('profileSetUsername', () => {
 
+    // with our DemoApiFunctionContextFixture, we can easily create a new user for this test case.
     demoAuthorizedUserContext(f, (u) => {
 
+      // jest it - test setting the username successfully.
       it('should set the profile username.', async () => {
+
         const username = 'username';
-        const fn = f.wrapCloudFunction(profileSetUsername(f.nestAppPromiseGetter));
+
+        // Build our profileSetUsername function using our testing context's Nest App, and the profileSetUsername factory.
+        const profileSetUsernameFn = profileSetUsername(f.nestAppPromiseGetter);
+
+        // wrap the function to make it a usable function.
+        const profileSetUsernameCloudFn = f.wrapCloudFunction(profileSetUsernameFn);
 
         const params: SetProfileUsernameParams = {
           username
         };
 
-        const result = await u.callCloudFunction(fn, params);
+        // Call the function using our user instance.
+        // This automatically creates a look-alike token/context to pass to the function so we don't have to mock that directly.
+        await u.callCloudFunction(profileSetUsernameCloudFn, params);
 
+        // Check our results.
         const profileDocument = u.instance.loadUserProfile();
         const profileDocumentSnapshot = await profileDocument.snapshot();
 
