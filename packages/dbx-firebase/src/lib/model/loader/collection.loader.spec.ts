b857cec908fd79a728b8e5bbea5c7cc4e6118e5d
Derek Burgman <derek@dereekb.com>
2022-05-05 01:17:23 -0500

    checkpoint: collection loader test fix

--- packages/dbx-firebase/src/lib/model/loader/collection.loader.spec.ts
+++ packages/dbx-firebase/src/lib/model/loader/collection.loader.spec.ts
@@ -3,6 +3,7 @@
  */
 // use the node environment, as the jsdom environment breaks for tests that use the firestore.
 
+import { SubscriptionObject } from '@dereekb/rxjs';
 import { authorizedTestWithMockItemCollection, MockItem, MockItemDocument } from "@dereekb/firebase";
 import { first } from "rxjs";
 import { DbxFirebaseCollectionLoaderInstance, dbxFirebaseCollectionLoaderInstanceWithCollection } from "./collection.loader.instance";
@@ -12,13 +13,16 @@ describe('DbxFirebaseCollectionLoaderInstance', () => {
   authorizedTestWithMockItemCollection((f) => {
 
     let instance: DbxFirebaseCollectionLoaderInstance<MockItem, MockItemDocument>;
+    let sub: SubscriptionObject;
 
     beforeEach(() => {
       const firestoreCollection = f.instance.firestoreCollection;
+      sub = new SubscriptionObject();
       instance = dbxFirebaseCollectionLoaderInstanceWithCollection(firestoreCollection);
     });
 
     afterEach(() => {
+      sub.destroy();
       instance.destroy();
     });
 
@@ -26,7 +30,7 @@ describe('DbxFirebaseCollectionLoaderInstance', () => {
 
       it('firestoreIteration$ should return the current iteration.', (done) => {
 
-        instance.firestoreIteration$.pipe(first()).subscribe((x) => {
+        sub.subscription = instance.firestoreIteration$.pipe(first()).subscribe((x) => {
           expect(x).toBeDefined();
           done();
         });
@@ -35,7 +39,7 @@ describe('DbxFirebaseCollectionLoaderInstance', () => {
 
       it('should return the current accumulator.', (done) => {
 
-        instance.accumulator$.pipe(first()).subscribe((x) => {
+        sub.subscription = instance.accumulator$.pipe(first()).subscribe((x) => {
           expect(x).toBeDefined();
           done();
         });
