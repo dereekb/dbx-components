8b1945e4d1161a92a458f93a5f9c019de09e8000
Derek Burgman <derek@dereekb.com>
2022-05-14 05:09:42 -0500

    build: added run-tests to the nx cacheable operations

--- packages/firebase-server/src/lib/firestore/driver.spec.ts
+++ packages/firebase-server/src/lib/firestore/driver.spec.ts
@@ -1,6 +1,8 @@
 import { describeQueryDriverTests, describeAccessorDriverTests } from "@dereekb/firebase/test";
 import { adminTestWithMockItemCollection } from "@dereekb/firebase-server/test";
 
+jest.setTimeout(9000);
+
 describe('firestore server', () => {
 
   adminTestWithMockItemCollection((f) => {
