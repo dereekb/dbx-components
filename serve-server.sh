0e6fb186add4dc003660d4501200de40ca911b20
Derek Burgman <derek@dereekb.com>
2022-05-19 18:07:24 -0500

    feat: improved serve-server
    - docker-compose now serves the server and can be spun up or down
    - watch-emulators now waits for ports to close before running the emulator

--- serve-server.sh
+++ serve-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Running server in docker container with emulators with continuous build enabled..."
-docker compose run --rm --service-ports demo-api-server npx nx serve demo-api
+docker-compose -f ./docker-compose.yml up
