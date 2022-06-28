65f667015d3cfd0c81e119eb06bd08993e7a0500
Derek Burgman <derek@dereekb.com>
2022-06-28 12:50:58 -0500

    refactor: removed default maxs from textField, textAreaField

--- packages/dbx-form/src/lib/formly/field/value/text/text.field.ts
+++ packages/dbx-form/src/lib/formly/field/value/text/text.field.ts
@@ -14,7 +14,7 @@ export interface TextFieldConfig extends LabeledFieldConfig, DescriptionFieldCon
 }
 
 export function textField(config: TextFieldConfig): FormlyFieldConfig {
-  const { key, pattern, minLength, maxLength = 1000, inputType: type = 'text' } = config;
+  const { key, pattern, minLength, maxLength, inputType: type = 'text' } = config;
   return formlyField({
     key,
     type: 'input',
@@ -32,7 +32,7 @@ export interface TextAreaFieldConfig extends LabeledFieldConfig, DescriptionFiel
 }
 
 export function textAreaField(config: TextAreaFieldConfig): FormlyFieldConfig {
-  const { key, rows = 3, minLength, maxLength = 1000 } = config;
+  const { key, rows = 3, minLength, maxLength } = config;
   return formlyField({
     key,
     type: 'textarea',
