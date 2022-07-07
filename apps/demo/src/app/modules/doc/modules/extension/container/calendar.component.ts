f9639863a9cfe599525604c7b8c2d1dcb513ee0d
Derek Burgman <derek@dereekb.com>
2022-07-07 00:29:25 -0500

    feat: added dbxCalendar Styling
    - Now styled with relation to the rest of the dbx-web styling

--- apps/demo/src/app/modules/doc/modules/extension/container/calendar.component.ts
+++ apps/demo/src/app/modules/doc/modules/extension/container/calendar.component.ts
@@ -21,41 +21,46 @@ export class DocExtensionCalendarComponent implements OnInit {
   constructor(readonly calendarStore: DbxCalendarStore<TestCalendarEventData>) {}
 
   ngOnInit(): void {
-    const now = new Date();
-    const days = 90;
-    const startsAt = setHours(addMonths(now, -1), 12);
+    function makeEvents(name: string, iFilter = 1, minutes = 90) {
+      const now = new Date();
+      const days = 90;
+      const startsAt = setHours(addMonths(now, -1), 12);
 
-    const timing = dateBlockTiming({ startsAt, duration: 10 }, days);
-    const eventData: TestCalendarEventData[] = range(0, days).map((i) => {
-      const event: TestCalendarEventData = {
-        i,
-        value: `${i}`
-      };
-
-      return event;
-    });
-
-    const dateBlockCollection: DateBlockCollection<TestCalendarEventData> = {
-      timing,
-      blocks: eventData
-    };
-
-    const spans = expandDateBlockCollection(dateBlockCollection);
-    const events: CalendarEvent<TestCalendarEventData>[] = spans
-      .filter((x) => x.i % 2)
-      .map((x) => {
-        const { start, end } = durationSpanToDateRange(x);
-        const title = `Event ${x.i}`;
-
-        return {
-          id: x.i,
-          title,
-          start,
-          end,
-          meta: x
+      const timing = dateBlockTiming({ startsAt, duration: minutes }, days);
+      const eventData: TestCalendarEventData[] = range(0, days).map((i) => {
+        const event: TestCalendarEventData = {
+          i,
+          value: `${i}`
         };
+
+        return event;
       });
 
+      const dateBlockCollection: DateBlockCollection<TestCalendarEventData> = {
+        timing,
+        blocks: eventData
+      };
+
+      const spans = expandDateBlockCollection(dateBlockCollection);
+      const events: CalendarEvent<TestCalendarEventData>[] = spans
+        .filter((x) => x.i % iFilter)
+        .map((x) => {
+          const { start, end } = durationSpanToDateRange(x);
+          const title = `Event ${name} ${x.i}`;
+
+          return {
+            id: x.i,
+            title,
+            start,
+            end,
+            meta: x
+          };
+        });
+
+      return events;
+    }
+
+    const events = [...makeEvents('A', 1), ...makeEvents('B', 2, 120), ...makeEvents('C', 3, 200), ...makeEvents('D', 5, 360), ...makeEvents('E', 5, 600), ...makeEvents('F', 6, 30), ...makeEvents('G', 7, 30)];
     this.calendarStore.setEvents(events);
   }
 }
