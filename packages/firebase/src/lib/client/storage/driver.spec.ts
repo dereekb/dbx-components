82e76202a930144be80cf910bdca1109931e7ef9
Derek Burgman <derek@dereekb.com>
2022-07-10 02:02:52 -0500

    demo: storage test
    - storage test for https://github.com/firebase/firebase-tools/issues/4716

--- packages/firebase/src/lib/client/storage/driver.spec.ts
+++ packages/firebase/src/lib/client/storage/driver.spec.ts
@@ -1,7 +1,29 @@
 import { authorizedTestWithMockItemStorage, describeFirebaseStorageAccessorDriverTests } from '@dereekb/firebase/test';
+import { getApp, getApps, initializeApp } from '@firebase/app';
+import { connectStorageEmulator, getStorage, ref, getBytes, uploadString, FirebaseStorage } from '@firebase/storage';
 
 describe('firebase storage client', () => {
   authorizedTestWithMockItemStorage((f) => {
     describeFirebaseStorageAccessorDriverTests(f);
+  });
+});
+
+describe('other tests', () => {
+  authorizedTestWithMockItemStorage((f) => {
+    it('should test', async () => {
+      const storage = f.storage as FirebaseStorage;
+
+      const stateRef = ref(storage, `test/data`);
+      await uploadString(stateRef, 'test');
+
+      console.log('Success.');
+
+      const result = await getBytes(stateRef);
+      const decoded = Buffer.from(result).toString('utf-8');
+      expect(result).toBeDefined();
+      expect(decoded).toBe('test');
+
+      console.log('Decoded: ', decoded);
+    });
   });
 });
