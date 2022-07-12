ee055f27a6be728250758a60e62dc5ac6ad22b98
Derek Burgman <derek@dereekb.com>
2022-07-11 21:20:42 -0500

    refactor: added preAssert to onCall crud functions
    - added preAssert function to allow synchronous assertions before the request occurs

--- packages/firebase-server/src/lib/nest/model/create.model.function.ts
+++ packages/firebase-server/src/lib/nest/model/create.model.function.ts
@@ -3,6 +3,7 @@ import { FirestoreModelType, FirestoreModelIdentity, FirestoreModelTypes, OnCall
 import { badRequestError } from '../../function';
 import { OnCallWithAuthorizedNestContext } from '../function/call';
 import { NestContextCallableRequestWithAuth } from '../function/nest';
+import { AssertModelCrudRequestFunction } from './crud.assert.function';
 
 // MARK: Function
 export type OnCallCreateModelFunction<N, I = unknown, O extends OnCallCreateModelResult = OnCallCreateModelResult> = (request: NestContextCallableRequestWithAuth<N, I> & ModelFirebaseCrudFunctionSpecifierRef) => PromiseOrValue<O>;
@@ -11,21 +12,29 @@ export type OnCallCreateModelMap<N, T extends FirestoreModelIdentity = Firestore
   [K in FirestoreModelTypes<T>]?: OnCallCreateModelFunction<N, any, OnCallCreateModelResult>;
 };
 
+export interface OnCallCreateModelConfig<N> {
+  preAssert?: AssertModelCrudRequestFunction<N, OnCallCreateModelParams>;
+}
+
 /**
  * Creates a OnCallWithAuthorizedNestContext function for creating a model.
  *
  * @param map
  * @returns
  */
-export function onCallCreateModel<N>(map: OnCallCreateModelMap<N>): OnCallWithAuthorizedNestContext<N, OnCallCreateModelParams, OnCallCreateModelResult> {
+export function onCallCreateModel<N>(map: OnCallCreateModelMap<N>, config: OnCallCreateModelConfig<N> = {}): OnCallWithAuthorizedNestContext<N, OnCallCreateModelParams, OnCallCreateModelResult> {
+  const { preAssert = () => undefined } = config;
+
   return (request) => {
     const modelType = request.data?.modelType;
     const createFn = map[modelType];
 
     if (createFn) {
+      const specifier = request.data.specifier;
+      preAssert({ crud: 'create', request, modelType, specifier });
       return createFn({
         ...request,
-        specifier: request.data.specifier,
+        specifier,
         data: request.data.data
       });
     } else {
--- packages/firebase-server/src/lib/nest/model/update.model.function.ts
+++ packages/firebase-server/src/lib/nest/model/update.model.function.ts
@@ -3,6 +3,7 @@ import { FirestoreModelType, FirestoreModelIdentity, FirestoreModelTypes, OnCall
 import { badRequestError } from '../../function';
 import { OnCallWithAuthorizedNestContext } from '../function/call';
 import { NestContextCallableRequestWithAuth } from '../function/nest';
+import { AssertModelCrudRequestFunction } from './crud.assert.function';
 
 // MARK: Function
 export type OnCallUpdateModelFunction<N, I = unknown, O = void> = (request: NestContextCallableRequestWithAuth<N, I> & ModelFirebaseCrudFunctionSpecifierRef) => PromiseOrValue<O>;
@@ -11,21 +12,29 @@ export type OnCallUpdateModelMap<N, T extends FirestoreModelIdentity = Firestore
   [K in FirestoreModelTypes<T>]?: OnCallUpdateModelFunction<N, any, any>;
 };
 
+export interface OnCallUpdateModelConfig<N> {
+  preAssert?: AssertModelCrudRequestFunction<N, OnCallUpdateModelParams>;
+}
+
 /**
  * Creates a OnCallWithAuthorizedNestContext function for updating model params.
  *
  * @param map
  * @returns
  */
-export function onCallUpdateModel<N>(map: OnCallUpdateModelMap<N>): OnCallWithAuthorizedNestContext<N, OnCallUpdateModelParams, unknown> {
+export function onCallUpdateModel<N>(map: OnCallUpdateModelMap<N>, config: OnCallUpdateModelConfig<N> = {}): OnCallWithAuthorizedNestContext<N, OnCallUpdateModelParams, unknown> {
+  const { preAssert = () => undefined } = config;
+
   return (request) => {
     const modelType = request.data?.modelType;
     const updateFn = map[modelType];
 
     if (updateFn) {
+      const specifier = request.data.specifier;
+      preAssert({ crud: 'update', request, modelType, specifier });
       return updateFn({
         ...request,
-        specifier: request.data.specifier,
+        specifier,
         data: request.data.data
       });
     } else {
