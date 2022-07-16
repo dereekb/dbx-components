0c82ffbdbce1f5b018304c03a2c3c73c1b41d381
Derek Burgman <derek@dereekb.com>
2022-07-16 01:50:24 -0500

    minor: added DayOfWeek

---
+++ packages/util/src/lib/date/week.ts
@@ -0,0 +1,25 @@
+export type Sunday = 0;
+export type Monday = 1;
+export type Tuesday = 2;
+export type Wednesday = 3;
+export type Thusrsday = 4;
+export type Friday = 5;
+export type Saturday = 6;
+
+/**
+ * Values that correspond to each day of the week.
+ */
+export type DayOfWeek = Sunday | Monday | Tuesday | Wednesday | Thusrsday | Friday | Saturday | Sunday;
+
+/**
+ * Enum for the days of the week.
+ */
+export enum Day {
+  SUNDAY = 0,
+  MONDAY = 1,
+  TUESDAY = 2,
+  WEDNESDAY = 3,
+  THURSDAY = 4,
+  FRIDAY = 5,
+  SATURDAY = 6
+}
