bbc3af3a6fd11105f09766cd4210f28940dbdd7a
Derek Burgman <derek@dereekb.com>
2022-08-14 15:14:35 -0500

    minor: added WebsiteLink class for class-validator

--- packages/model/src/lib/data/website/link.ts
+++ packages/model/src/lib/data/website/link.ts
@@ -1,4 +1,6 @@
 import { ModelTypeString } from '@dereekb/util';
+import { Expose } from 'class-transformer';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export type WebsiteLinkType = ModelTypeString;
 
@@ -15,4 +17,33 @@ export interface WebsiteLink {
    * Encoded website link data
    */
   d: WebsiteLinkEncodedData;
+}
+
+/**
+ * Default max length of WebsiteLink's data type.
+ */
+export const WEBSITE_LINK_TYPE_MAX_LENGTH = 32;
+
+/**
+ * Default max length of WebsiteLink's data string.
+ */
+export const WEBSITE_LINK_ENCODED_DATA_MAX_LENGTH = 1000;
+
+export class WebsiteLink {
+  @Expose()
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(WEBSITE_LINK_TYPE_MAX_LENGTH)
+  t: WebsiteLinkType;
+
+  @Expose()
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(WEBSITE_LINK_ENCODED_DATA_MAX_LENGTH)
+  d: WebsiteLinkEncodedData;
+
+  constructor(template: WebsiteLink) {
+    this.t = template.t;
+    this.d = template.d;
+  }
 }
