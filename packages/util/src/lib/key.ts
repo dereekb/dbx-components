e917c11d4575b20afd1bcd46ce925ce5729728df
Derek Burgman <derek@dereekb.com>
2022-09-06 13:02:15 -0500

    minor: added distrinctUntilKeysChange()

--- packages/util/src/lib/key.ts
+++ packages/util/src/lib/key.ts
@@ -1,3 +1,5 @@
+import { MapArrayFunction, MapFunction } from './value/map';
+import { mapArrayFunction } from './value';
 import { Maybe } from './value/maybe.type';
 
 /**
@@ -10,5 +12,54 @@ export type PrimativeKey = string | number;
  */
 export type FieldOfType<T> = keyof T;
 
-export type ReadKeyFunction<T, K extends PrimativeKey = PrimativeKey> = (model: T) => Maybe<K>;
-export type ReadKeysFunction<T, K extends PrimativeKey = PrimativeKey> = (model: T) => K[];
+/**
+ * Reads a key from the input object.
+ */
+export type ReadKeyFunction<T, K extends PrimativeKey = PrimativeKey> = MapFunction<T, Maybe<K>>;
+
+/**
+ * Reads all defined keys from the input objects.
+ */
+export type ReadKeysFunction<T, K extends PrimativeKey = PrimativeKey> = MapFunction<T[], K[]>;
+
+/**
+ * Creates a ReadKeysFunction using a ReadKeyFunction.
+ *
+ * @param read
+ */
+export function readKeysFunction<T, K extends PrimativeKey = PrimativeKey>(readKey: ReadKeyFunction<T, K>): ReadKeysFunction<T, K> {
+  return (values: T[]) => {
+    const keys: K[] = new Array(values.length);
+
+    values.forEach((x) => {
+      const key = readKey(x);
+
+      if (key != null) {
+        keys.push(key);
+      }
+    });
+
+    return keys;
+  };
+}
+
+/**
+ * Reads all defined keys from the input objects to a Set.
+ */
+export type ReadKeysSetFunction<T, K extends PrimativeKey = PrimativeKey> = MapFunction<T[], Set<K>>;
+
+export function readKeysSetFunction<T, K extends PrimativeKey = PrimativeKey>(readKey: ReadKeyFunction<T, K>): ReadKeysSetFunction<T, K> {
+  return (values: T[]) => {
+    const keys = new Set<K>();
+
+    values.forEach((x) => {
+      const key = readKey(x);
+
+      if (key != null) {
+        keys.add(key);
+      }
+    });
+
+    return keys;
+  };
+}
