796729a50d38b84458948cdb0d90f4fc7d6200ee
Derek Burgman <derek@dereekb.com>
2022-09-16 00:29:15 +0000

    checkpoint: mailgun

---
+++ packages/nestjs/mailgun/jest.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+(global as any).appTestType = 'nestjs';
+(global as any).testFolderRootPath = '<rootDir>/../../..';
+
+module.exports = {
+  displayName: 'nestjs-mailgun',
+  preset: '../../../jest.preset.ts',
+  coverageDirectory: '../../../coverage/packages/nestjs/mailgun'
+};
