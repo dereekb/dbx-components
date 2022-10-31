fb75a09ee6c6e3b82a721af22a770c2e28e426ff
Derek Burgman <derek@dereekb.com>
2022-10-31 01:18:01 -0500

    refactor: added invertDecision()

--- packages/util/src/lib/value/decision.ts
+++ packages/util/src/lib/value/decision.ts
@@ -1,3 +1,4 @@
+import { FilterFunction, invertFilter } from '../filter/filter';
 import { FactoryWithRequiredInput } from '../getter/getter';
 import { MapFunction, AsyncMapFunction } from './map';
 
@@ -8,3 +9,14 @@ export type DecisionFunction<I> = MapFunction<I, boolean>;
 export type AsyncDecisionFunction<I> = AsyncMapFunction<DecisionFunction<I>>;
 
 export type DecisionFunctionFactory<C, I> = FactoryWithRequiredInput<DecisionFunction<I>, C>;
+
+/**
+ * Used to invert a decision function by returning the opposite of what it returns.
+ *
+ * @param filterFn
+ * @param invert whether or not to apply the inversion.
+ * @returns
+ */
+export function invertDecision<T = unknown, F extends DecisionFunction<T> = DecisionFunction<T>>(decisionFn: F, invert = true): F {
+  return invertFilter(decisionFn as FilterFunction, invert) as F;
+}
