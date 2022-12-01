392202f854d2500882a85bbf94d1e543523b4cc3
Derek Burgman <derek@dereekb.com>
2022-11-30 22:14:00 -0600

    refactor: added mergeRequestInits()
    - fetchRequestFactory() will now merge headers together properly instead of replacing the object entirely

--- packages/util/src/lib/map/map.ts
+++ packages/util/src/lib/map/map.ts
@@ -1,5 +1,6 @@
-import { ArrayOrValue, asArray } from '../array';
-import { Maybe } from '../value';
+import { ArrayOrValue, asArray } from '../array/array';
+import { forEachInIterable, useIterableOrValue } from '../iterable/iterable';
+import { Maybe } from '../value/maybe.type';
 
 /**
  *
@@ -27,4 +28,42 @@ export function combineMaps<K, T>(...maps: Maybe<Map<K, T>>[]): Map<K, T> {
 export function setKeysOnMap<K, T>(map: Map<K, T>, key: ArrayOrValue<K>, value: T): Map<K, T> {
   asArray(key).forEach((key) => map.set(key, value));
   return map;
+}
+
+/**
+ * Returns the array of entries from the map as tuples.
+ *
+ * @param map
+ * @returns
+ */
+export function mapToTuples<K, T>(map: Map<K, T>): [K, T][] {
+  return Array.from(map.entries());
+}
+
+/**
+ * Expands a map that has array values into key/value tuples.
+ *
+ * @param map
+ * @returns
+ */
+export function expandArrayMapTuples<K, T>(map: Map<K, T[]>): [K, T][] {
+  return expandArrayValueTuples(mapToTuples(map));
+}
+
+/**
+ * Expands the input tuples that may be an ArrayOrValue into tuples that are only key/value tuples.
+ *
+ * @param values
+ * @returns
+ */
+export function expandArrayValueTuples<K, T>(values: [K, ArrayOrValue<T>][]): [K, T][] {
+  const tuples: [K, T][] = [];
+
+  values.forEach(([key, values]) => {
+    useIterableOrValue(values, (value) => {
+      tuples.push([key, value]);
+    });
+  });
+
+  return tuples;
 }
