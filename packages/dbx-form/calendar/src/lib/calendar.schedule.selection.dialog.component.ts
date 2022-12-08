e5edf264fce49351f06428fab1334fccead55f2b
Derek Burgman <derek@dereekb.com>
2022-12-08 00:54:40 -0600

    refactor: selection dialog mobile size fix

--- packages/dbx-form/calendar/src/lib/calendar.schedule.selection.dialog.component.ts
+++ packages/dbx-form/calendar/src/lib/calendar.schedule.selection.dialog.component.ts
@@ -8,7 +8,7 @@ export interface DbxScheduleSelectionCalendarDatePopupConfig {
 
 @Component({
   template: `
-    <dbx-dialog-content>
+    <dbx-dialog-content class="dbx-schedule-selection-calendar-date-dialog">
       <dbx-dialog-content-close (close)="close()"></dbx-dialog-content-close>
       <dbx-schedule-selection-calendar></dbx-schedule-selection-calendar>
       <dbx-dialog-content-footer closeText="Close Calendar" (close)="close()"></dbx-dialog-content-footer>
@@ -19,8 +19,10 @@ export class DbxScheduleSelectionCalendarDateDialogComponent extends AbstractDia
   static openDialog(matDialog: MatDialog, { injector }: DbxScheduleSelectionCalendarDatePopupConfig) {
     return matDialog.open(DbxScheduleSelectionCalendarDateDialogComponent, {
       injector,
+      height: 'calc(var(--vh100) * 0.9)',
+      minHeight: 400,
       width: '80vw',
-      minWidth: 460
+      minWidth: 360
     });
   }
 }
