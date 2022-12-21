44375d56b9a3d4e75aaba06300a55c4405b29e64
Derek Burgman <derek@dereekb.com>
2022-12-21 15:04:28 -0600

    refactor: added redirectForIdentifierParamHook()

--- packages/dbx-core/src/lib/router/auth/hook/uid.hook.ts
+++ packages/dbx-core/src/lib/router/auth/hook/uid.hook.ts
@@ -1,9 +1,10 @@
 import { AuthUserIdentifier } from '../../../auth/auth.user';
 import { DbxAuthService } from '../../../auth/service/auth.service';
-import { TransitionService, TransitionHookFn, Transition, HookResult, StateService, HookMatchCriteria } from '@uirouter/core';
+import { TransitionService, HookMatchCriteria } from '@uirouter/core';
 import { Maybe } from '@dereekb/util';
-import { map, first, switchMap, firstValueFrom, Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Injector } from '@angular/core';
+import { redirectForIdentifierParamHook } from './id.hook';
 
 export const DEFAULT_REDIRECT_FOR_USER_IDENTIFIER_PARAM_VALUE = '0';
 export const DEFAULT_REDIRECT_FOR_USER_IDENTIFIER_PARAM_KEY = 'uid';
@@ -40,66 +41,17 @@ export interface RedirectForUserIdentifierParamHookInput {
 
 /**
  * This hook asserts the user is allowed to view a route with a user identifier as a parameter.
- *
- * If not, or
  */
 export function redirectForUserIdentifierParamHook(input: RedirectForUserIdentifierParamHookInput): void {
-  const { uidParam = DEFAULT_REDIRECT_FOR_USER_IDENTIFIER_PARAM_KEY, defaultParamValue = DEFAULT_REDIRECT_FOR_USER_IDENTIFIER_PARAM_VALUE, priority = 100, transitionService, canViewUser } = input;
-  const criteria: HookMatchCriteria = typeof input.criteria === 'string' ? { entering: input.criteria } : input.criteria;
+  const { uidParam = DEFAULT_REDIRECT_FOR_USER_IDENTIFIER_PARAM_KEY, defaultParamValue = DEFAULT_REDIRECT_FOR_USER_IDENTIFIER_PARAM_VALUE, criteria, priority = 100, transitionService, canViewUser } = input;
 
-  // https://ui-router.github.io/ng2/docs/latest/modules/transition.html#hookresult
-  const assertAllowedUid: TransitionHookFn = (transition: Transition): HookResult => {
-    const $state: StateService = transition.router.stateService;
-    const injector = transition.injector();
-    const authService: DbxAuthService = injector.get(DbxAuthService);
-    const params = transition.params();
-
-    const transitionTargetUid: Maybe<string> = params[uidParam];
-
-    return firstValueFrom(
-      authService.userIdentifier$.pipe(
-        first(),
-        switchMap((currentUserId) => {
-          let result: Observable<HookResult> = of(true);
-
-          let redirectToUid: Maybe<Observable<Maybe<AuthUserIdentifier>>>;
-
-          if (!transitionTargetUid || transitionTargetUid === defaultParamValue) {
-            // If uid isn't set, default to the current user.
-            redirectToUid = of(currentUserId);
-          } else if (currentUserId !== transitionTargetUid) {
-            redirectToUid = canViewUser(transitionTargetUid, authService, injector).pipe(
-              map((x) => {
-                if (x == null || typeof x === 'boolean') {
-                  return x ? transitionTargetUid : currentUserId;
-                } else {
-                  return x;
-                }
-              })
-            );
-          }
-
-          if (redirectToUid != null) {
-            result = redirectToUid.pipe(
-              first(),
-              map((targetUid) => {
-                if (targetUid !== transitionTargetUid) {
-                  const target = transition.targetState();
-                  const state = target.state();
-                  return $state.target(state, { ...params, uid: targetUid }, { location: true });
-                } else {
-                  return true;
-                }
-              })
-            );
-          }
-
-          return result;
-        })
-      )
-    ) as HookResult;
-  };
-
-  // Register the "requires auth" hook with the TransitionsService
-  transitionService.onBefore(criteria, assertAllowedUid, { priority });
+  return redirectForIdentifierParamHook({
+    defaultAllowedValue: (authService: DbxAuthService) => authService.userIdentifier$,
+    idParam: uidParam,
+    defaultParamValue,
+    criteria,
+    transitionService,
+    canViewModelWithId: canViewUser,
+    priority
+  });
 }
