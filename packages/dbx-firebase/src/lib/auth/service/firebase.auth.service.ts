2d18dacf2ecf704738d71c1bff550df635217b83
ci <ci@dereekb.com>
2022-12-26 06:02:57 +0000

    release(workspace): v9.20.5 release
    refactor: added AbstractRootSingleItemDbxFirebaseDocument
    
    refactor: added RootSingleItemFirestoreCollection
    
    minor: added reauthenticateWithPopup()

--- packages/dbx-firebase/src/lib/auth/service/firebase.auth.service.ts
+++ packages/dbx-firebase/src/lib/auth/service/firebase.auth.service.ts
@@ -1,11 +1,11 @@
 import { filterMaybe, isNot, timeoutStartWith } from '@dereekb/rxjs';
 import { Injectable, Optional } from '@angular/core';
 import { AuthUserState, DbxAuthService, loggedOutObsFromIsLoggedIn, loggedInObsFromIsLoggedIn, AuthUserIdentifier, authUserIdentifier } from '@dereekb/dbx-core';
-import { Auth, authState, idToken, User, IdTokenResult, ParsedToken, GoogleAuthProvider, signInWithPopup, AuthProvider, PopupRedirectResolver, signInAnonymously, signInWithEmailAndPassword, UserCredential, FacebookAuthProvider, GithubAuthProvider, TwitterAuthProvider, createUserWithEmailAndPassword } from '@angular/fire/auth';
+import { reauthenticateWithCredential, reauthenticateWithPopup, reauthenticateWithRedirect, reauthenticateWithPhoneNumber, Auth, authState, idToken, User, IdTokenResult, ParsedToken, GoogleAuthProvider, signInWithPopup, AuthProvider, PopupRedirectResolver, signInAnonymously, signInWithEmailAndPassword, UserCredential, FacebookAuthProvider, GithubAuthProvider, TwitterAuthProvider, createUserWithEmailAndPassword } from '@angular/fire/auth';
 import { of, Observable, distinctUntilChanged, shareReplay, map, switchMap, firstValueFrom, catchError } from 'rxjs';
 import { AuthClaims, AuthClaimsObject, AuthRoleClaimsService, AuthRoleSet, AUTH_ADMIN_ROLE, cachedGetter, Maybe } from '@dereekb/util';
 import { AuthUserInfo, authUserInfoFromAuthUser, firebaseAuthTokenFromUser } from '../auth';
-import { sendPasswordResetEmail } from 'firebase/auth';
+import { AuthCredential, sendPasswordResetEmail } from 'firebase/auth';
 import { authUserStateFromFirebaseAuthServiceFunction } from './firebase.auth.rxjs';
 import { FirebaseAuthContextInfo, FirebaseAuthToken } from '@dereekb/firebase';
 
@@ -210,6 +210,20 @@ export class DbxFirebaseAuthService implements DbxAuthService {
 
   logOut(): Promise<void> {
     return this.firebaseAuth.signOut();
+  }
+
+  reauthenticateWithPopup(provider: AuthProvider, resolver?: PopupRedirectResolver): Promise<UserCredential> {
+    return firstValueFrom(
+      this.currentAuthUser$.pipe(
+        switchMap((x: Maybe<User>) => {
+          if (x) {
+            return reauthenticateWithPopup(x, provider, resolver);
+          } else {
+            throw new Error('User is not logged in currently.');
+          }
+        })
+      )
+    );
   }
 }
 
--- packages/firebase/src/lib/common/firestore/collection/index.ts
+++ packages/firebase/src/lib/common/firestore/collection/index.ts
@@ -1,5 +1,6 @@
 export * from './collection';
 export * from './collection.key';
+export * from './collection.single';
 export * from './collection.group';
 export * from './subcollection';
 export * from './subcollection.single';
