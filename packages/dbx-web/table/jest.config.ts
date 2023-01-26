fd92627aea714ce63629959f48f1548a89df1ed8
Derek Burgman <derek@dereekb.com>
2023-01-25 18:54:25 -0600

    checkpoint: outlined @dbx-web/table

---
+++ packages/dbx-web/table/jest.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+(global as any).appTestType = 'angular';
+(global as any).testFolderRootPath = '<rootDir>/../../..';
+
+module.exports = {
+  displayName: 'dbx-web-table',
+  preset: '../../../jest.preset.ts',
+  coverageDirectory: '../../../../coverage/packages/dbx-web/table'
+};
