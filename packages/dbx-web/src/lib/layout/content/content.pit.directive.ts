cbce68ad73dc896acc34232e3375698133e99241
Derek Burgman <derek@dereekb.com>
2023-01-31 15:15:11 -0600

    feat: added dbx-content-pit

---
+++ packages/dbx-web/src/lib/layout/content/content.pit.directive.ts
@@ -0,0 +1,12 @@
+import { Component, Directive, Input } from '@angular/core';
+
+/**
+ * Component used to wrap content in a pit with a label.
+ */
+@Directive({
+  selector: 'dbx-content-pit, [dbxContentPit]',
+  host: {
+    class: 'd-block dbx-content-pit'
+  }
+})
+export class DbxContentPitDirective {}
