5f20e46a961ed15d43585c2d1f1a16df3f899e67
Derek Burgman <derek@dereekb.com>
2023-03-30 10:43:42 -0500

    refactor: added ZoomLevelRef

--- packages/util/src/lib/value/zoom.ts
+++ packages/util/src/lib/value/zoom.ts
@@ -4,6 +4,13 @@
 export type ZoomLevel = number;
 
 /**
+ * Reference to a zoom level
+ */
+export interface ZoomLevelRef {
+  z: ZoomLevel;
+}
+
+/**
  * Camera zoom level range.
  */
 export interface ZoomLevelRange {
