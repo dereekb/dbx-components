f945bfd42e5f6af09a870beb92f206dcbf270e6c
Derek Burgman <derek@dereekb.com>
2023-04-02 13:52:23 -0500

    refactor: added generic type to TypedModel

--- packages/util/src/lib/model/model.ts
+++ packages/util/src/lib/model/model.ts
@@ -28,8 +28,8 @@ export interface UniqueModel {
   id?: ModelKey;
 }
 
-export interface TypedModel {
-  type: ModelTypeString;
+export interface TypedModel<M extends ModelTypeString = ModelTypeString> {
+  type: M;
 }
 
 export interface NamedUniqueModel extends UniqueModel {
@@ -47,7 +47,7 @@ export type ModelOrKey<T> = T | ModelKey;
  */
 export type UniqueModelOrKey<T extends UniqueModel> = ModelOrKey<T>;
 
-export interface ModelKeyTypePair extends TypedModel, ModelKeyRef {}
+export interface ModelKeyTypePair<M extends ModelTypeString = ModelTypeString> extends TypedModel<M>, ModelKeyRef {}
 
 /**
  * An encoded ModelKeyTypePair.
@@ -58,7 +58,7 @@ export interface ModelKeyNamePair extends Pick<ModelKeyTypePair, 'key'> {
   name?: string;
 }
 
-export interface ModelKeyTypeNamePair extends ModelKeyNamePair, ModelKeyTypePair {}
+export interface ModelKeyTypeNamePair<M extends ModelTypeString = ModelTypeString> extends ModelKeyNamePair, ModelKeyTypePair<M> {}
 
 export interface ReadModelKeyParams<T> {
   required?: boolean;
@@ -66,7 +66,7 @@ export interface ReadModelKeyParams<T> {
 }
 
 export type ReadModelKeyFunction<T> = ReadKeyFunction<T, ModelKey>;
-export type ReadModelTypeFunction<T> = ReadKeyFunction<T, ModelTypeString>;
+export type ReadModelTypeFunction<T, M extends ModelTypeString = ModelTypeString> = ReadKeyFunction<T, M>;
 export type ReadRelationKeysFunction<T> = ReadMultipleKeysFunction<T, ModelKey>;
 
 export type MultiModelKeyMap<T> = Map<string, T>;
@@ -217,10 +217,10 @@ export function encodeModelKeyTypePair(pair: ModelKeyTypePair): ModelKey {
   return `${pair.type}_${pair.key}`;
 }
 
-export function decodeModelKeyTypePair(linkKey: ModelKey): ModelKeyTypePair {
+export function decodeModelKeyTypePair<M extends ModelTypeString = ModelTypeString>(linkKey: ModelKey): ModelKeyTypePair<M> {
   const split = linkKey.split('_');
   return {
-    type: split[0],
+    type: split[0] as M,
     key: split[1]
   };
 }
@@ -229,18 +229,18 @@ export function decodeModelKeyTypePair(linkKey: ModelKey): ModelKeyTypePair {
 /**
  * A type and data pair.
  */
-export interface ModelTypeDataPair<T = unknown> extends TypedModel {
+export interface ModelTypeDataPair<T = unknown, M extends ModelTypeString = ModelTypeString> extends TypedModel<M> {
   data: T;
 }
 
 /**
  * Used for converting the input data into a ModelTypeDataPair value.
  */
-export type ModelTypeDataPairFactory<T> = MapFunction<T, ModelTypeDataPair<T>>;
+export type ModelTypeDataPairFactory<T, M extends ModelTypeString = ModelTypeString> = MapFunction<T, ModelTypeDataPair<T, M>>;
 
-export function modelTypeDataPairFactory<T>(typeReader: ReadModelTypeFunction<T>, defaultType = DEFAULT_UNKNOWN_MODEL_TYPE_STRING): ModelTypeDataPairFactory<T> {
+export function modelTypeDataPairFactory<T, M extends ModelTypeString = ModelTypeString>(typeReader: ReadModelTypeFunction<T, M>, defaultType = DEFAULT_UNKNOWN_MODEL_TYPE_STRING): ModelTypeDataPairFactory<T, M> {
   return (data: T) => {
-    const type: ModelTypeString = typeReader(data) ?? defaultType;
+    const type: M = typeReader(data) ?? (defaultType as M);
     return {
       type,
       data
