a0877d0253b87449b3b8dde8514b02afb5a6209e
Derek Burgman <derek@dereekb.com>
2023-05-11 15:05:55 -0500

    refactor: added auth/wrong-password code handling
    - The auth now tells the user that their password was wrong.

--- packages/firebase/src/lib/common/auth/auth.error.ts
+++ packages/firebase/src/lib/common/auth/auth.error.ts
@@ -7,23 +7,32 @@ export interface FirebaseAuthError {
 }
 
 export const FIREBASE_AUTH_USER_NOT_FOUND_ERROR = 'auth/user-not-found';
+export const FIREBASE_AUTH_WRONG_PASSWORD = 'auth/wrong-password';
 export const FIREBASE_AUTH_NETWORK_REQUEST_ERROR = 'auth/network-request-error';
+export const FIREBASE_AUTH_NETWORK_REQUEST_FAILED = 'auth/network-request-failed';
 
 export function firebaseAuthErrorToReadableError(inputError: FirebaseAuthError): ReadableError {
   const code = inputError.code;
   let error: ReadableError;
 
   switch (code) {
+    case FIREBASE_AUTH_WRONG_PASSWORD:
+      error = {
+        code,
+        message: `The password you entered does not match our records. Please try again or click on "Forgot Password" to reset your password.`
+      };
+      break;
     case FIREBASE_AUTH_USER_NOT_FOUND_ERROR:
       error = {
         code,
         message: 'A user with this email address was not found.'
       };
       break;
+    case FIREBASE_AUTH_NETWORK_REQUEST_FAILED:
     case FIREBASE_AUTH_NETWORK_REQUEST_ERROR:
       error = {
         code,
-        message: 'Could not reach the server.'
+        message: 'Could not reach the server. Are you connected to the internet?'
       };
       break;
     default:
