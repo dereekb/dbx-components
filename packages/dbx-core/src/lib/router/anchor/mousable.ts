bf235ad777ba61802b3d0018a5696f835736becc
Derek Burgman <derek@dereekb.com>
2023-05-17 12:13:29 -0600

    refactor: added MouseEventPair

--- packages/dbx-core/src/lib/router/anchor/mousable.ts
+++ packages/dbx-core/src/lib/router/anchor/mousable.ts
@@ -2,6 +2,11 @@ import { MailToUrlInput, mailToUrlString, Maybe, PhoneNumber, telUrlString, Webs
 
 export type MouseEventType = 'enter' | 'leave';
 
+export interface MouseEventPair<T> {
+  readonly type: MouseEventType;
+  readonly data: T;
+}
+
 export interface MousableFunction {
   onMouse?: (type: MouseEventType, event?: Maybe<MouseEvent>) => void;
 }
