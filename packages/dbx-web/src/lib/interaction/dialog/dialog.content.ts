c3d8dec254ddb7e5d0219970d99cec28b7c73b29
Derek Burgman <derek@dereekb.com>
2023-06-26 20:35:22 -0500

    refactor: added sanitizeDbxDialogContentConfig()
    - fixed issue where a DbxDialogContentConfig that was passed had a single string containing all CSS classes that would cause the panel to break. Css classes are now converted to an array.

--- packages/dbx-web/src/lib/interaction/dialog/dialog.content.ts
+++ packages/dbx-web/src/lib/interaction/dialog/dialog.content.ts
@@ -1,3 +1,13 @@
 import { MatDialogConfig } from '@angular/material/dialog';
+import { Maybe, cssClassesSet } from '@dereekb/util';
 
 export type DbxDialogContentConfig = Omit<MatDialogConfig, 'viewContainerRef' | 'injector' | 'id' | 'data'>;
+
+export function sanitizeDbxDialogContentConfig(input: Maybe<DbxDialogContentConfig>): DbxDialogContentConfig {
+  const panelClass = input?.panelClass;
+
+  return {
+    ...input,
+    panelClass: panelClass ? Array.from(cssClassesSet(input?.panelClass)) : undefined
+  };
+}
