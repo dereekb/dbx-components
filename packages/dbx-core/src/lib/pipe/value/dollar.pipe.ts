71372889ecbf536efe8ab2a937c180582885e6e9
ci <ci@dereekb.com>
2023-08-17 19:17:24 +0000

    release(workspace): v9.24.31 release
    refactor: added DollarAmountPipe
    
    refactor: added fractional hour

---
+++ packages/dbx-core/src/lib/pipe/value/dollar.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { dollarAmountString, Maybe } from '@dereekb/util';
+
+/**
+ * Pipe that takes in a number and returns the number formatted as a dollar using dollarAmountString().
+ *
+ * Can provide a default string value to use when the input is null or undefined.
+ */
+@Pipe({ name: 'dollarAmount' })
+export class DollarAmountPipe implements PipeTransform {
+  transform(input: Maybe<number>, defaultIfNull?: Maybe<string>): Maybe<string> {
+    return defaultIfNull == null || input != null ? dollarAmountString(input) : defaultIfNull;
+  }
+}
---
+++ packages/util/src/lib/date/hour.ts
@@ -0,0 +1,64 @@
+import { cutValueToPrecisionFunction } from '../number/round';
+import { Maybe } from '../value/maybe.type';
+import { Hours, Minutes, MINUTES_IN_HOUR } from './date';
+
+/**
+ * A number that represents hours and rounded to the nearest minute.
+ *
+ * It has only three decimal places max.
+ *
+ * Fractional hours are safe to add together.
+ *
+ * For example:
+ *
+ * - 10 minutes: 0.16
+ * - 15 minutes : 0.25
+ */
+export type FractionalHour = number;
+
+export const FRACTIONAL_HOURS_PRECISION_FUNCTION = cutValueToPrecisionFunction(3);
+
+/**
+ * Converts the number of minnutes to a fractional hour.
+ *
+ * Minutes are rounded down.
+ */
+export function minutesToFractionalHours(minutes: Minutes): FractionalHour {
+  return FRACTIONAL_HOURS_PRECISION_FUNCTION(Math.floor(minutes) / MINUTES_IN_HOUR);
+}
+
+/**
+ * Converts the fractional hour to a minute.
+ */
+export function fractionalHoursToMinutes(hours: FractionalHour): Minutes {
+  return Math.round(hours * MINUTES_IN_HOUR);
+}
+
+/**
+ * Rounds the input hour to the nearest FractionalHour.
+ *
+ * @param hour
+ * @returns
+ */
+export function hourToFractionalHour(hour: Hours): FractionalHour {
+  return minutesToFractionalHours(fractionalHoursToMinutes(hour));
+}
+
+export interface ComputeFractionalHour {
+  readonly minutes?: Maybe<Minutes>;
+  readonly hours?: Maybe<Hours>;
+}
+
+export function computeNextFractionalHour(input: FractionalHour, change: ComputeFractionalHour): FractionalHour {
+  let result = input;
+
+  if (change.minutes) {
+    result += minutesToFractionalHours(change.minutes);
+  }
+
+  if (change.hours) {
+    result += hourToFractionalHour(change.hours);
+  }
+
+  return result;
+}
