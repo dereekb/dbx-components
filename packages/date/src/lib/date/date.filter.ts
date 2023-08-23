7f774540b5489822216d758b589abef2ff261bdd
ci <ci@dereekb.com>
2023-08-23 01:21:58 +0000

    release(workspace): v9.24.33 release
    refactor: added filters to DateScheduleDateBlockTimingFilterConfig

--- packages/date/src/lib/date/date.filter.ts
+++ packages/date/src/lib/date/date.filter.ts
@@ -1,17 +1,28 @@
 import { FilterFunction } from '@dereekb/util';
-import { isAfter } from 'date-fns';
+import { isAfter, isBefore } from 'date-fns';
 import { DateBlock, DateBlockDurationSpan } from './date.block';
 import { dateDurationSpanEndDate } from './date.duration';
 
 export type DateBlockDurationSpanFilterFunction<B extends DateBlock = DateBlock> = FilterFunction<DateBlockDurationSpan<B>>;
 
+export function dateBlockDurationSpanHasStartedFilterFunction<B extends DateBlock = DateBlock>(now = new Date()): DateBlockDurationSpanFilterFunction<B> {
+  return (x) => !isBefore(x.startsAt, now); // startsAt <= now
+}
+
 export function dateBlockDurationSpanHasNotStartedFilterFunction<B extends DateBlock = DateBlock>(now = new Date()): DateBlockDurationSpanFilterFunction<B> {
-  return (x) => isAfter(x.startsAt, now);
+  return (x) => isAfter(x.startsAt, now); // startsAt > now
+}
+
+export function dateBlockDurationSpanHasEndedFilterFunction<B extends DateBlock = DateBlock>(now = new Date()): DateBlockDurationSpanFilterFunction<B> {
+  return (x) => {
+    const endsAt = dateDurationSpanEndDate(x);
+    return !isBefore(endsAt, now); // endsAt <= now
+  };
 }
 
 export function dateBlockDurationSpanHasNotEndedFilterFunction<B extends DateBlock = DateBlock>(now = new Date()): DateBlockDurationSpanFilterFunction<B> {
   return (x) => {
     const endsAt = dateDurationSpanEndDate(x);
-    return isAfter(endsAt, now);
+    return isAfter(endsAt, now); // endsAt > now
   };
 }
