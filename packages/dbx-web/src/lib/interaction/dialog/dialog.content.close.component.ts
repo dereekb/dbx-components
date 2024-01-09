400692ddfed2fd93581ae56f93ad2a5ef6dacc3a
Derek Burgman <derek@dereekb.com>
2024-01-09 08:39:28 -0600

    refactor: dialog styling refactoring for Angular 16

--- packages/dbx-web/src/lib/interaction/dialog/dialog.content.close.component.ts
+++ packages/dbx-web/src/lib/interaction/dialog/dialog.content.close.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnDestroy, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
 
 /**
  * Component used to show a close button at the top of a dialog, floating in a corner.
@@ -6,13 +6,17 @@ import { Component, EventEmitter, OnDestroy, Output } from '@angular/core';
 @Component({
   selector: 'dbx-dialog-content-close',
   template: `
-    <button mat-icon-button (click)="closeClicked()"><mat-icon>close</mat-icon></button>
+    <button class="dbx-dialog-content-close-button" mat-icon-button (click)="closeClicked()"><mat-icon>close</mat-icon></button>
   `,
   host: {
-    class: 'dbx-dialog-content-close'
+    class: 'dbx-dialog-content-close',
+    '[class.dbx-dialog-content-close-padding]': 'padded'
   }
 })
 export class DbxDialogContentCloseComponent implements OnDestroy {
+  @Input()
+  padded = true;
+
   @Output()
   readonly close = new EventEmitter<void>();
 
