efa01b2df0b90586483ab68cfd200ef92df0a214
Derek Burgman <derek@dereekb.com>
2024-01-10 17:15:20 -0600

    build: fixed make-env.js

--- make-env.js
+++ make-env.js
@@ -3,7 +3,7 @@
  *
  * The expected usage is:
  *
- * node make-env.js <environment>
+ * node make-env.js <specifier>
  *
  * Example:
  *
@@ -17,20 +17,33 @@
 const fs = require('fs');
 const { parse, stringify } = require('envfile');
 
+const specifier = process.argv[2] || 'test';
+
+const envSpecifierType = new String(specifier).toUpperCase(); // Also use this to find variables. Always lowercase
+const envSpecifierTypeLower = envSpecifierType.toLowerCase();
+const envSpecifierSeparator = '_';
+
 // NOTE: If run within nx, remember that nx adds all variables within .env to the environment and, thus, process.env.
 // Variables that are within bash take prority.
 const templateFilePath = '.env';
 const templateEnv = fs.readFileSync(templateFilePath).toString();
 const template = parse(templateEnv);
 
-const env = {}; // this is the object that is parsed
+// Also attempt to read an overriding template file with the name .env.<environment>
+// Any variables from here are not added to the template; they're just used for overriding variables in the template.
+// They are the lowest level of defaults.
+const specifierTemplateOverridesFilePath = `.env.${envSpecifierTypeLower}`;
+let specifierTemplateOverrides = {};
+
+if (fs.existsSync(specifierTemplateOverridesFilePath)) {
+  const specifierTemplateOverridesEnv = fs.readFileSync(specifierTemplateOverridesFilePath).toString();
+  specifierTemplateOverrides = parse(specifierTemplateOverridesEnv);
+}
+
+const env = {}; // this is the object that is exported
 
 const keysToIgnoreFromTemplate = ['PUT_YOUR_REAL_SECRETS_INTO_ENV_SECRET', 'THIS_FILE_IS_COMMITTED_TO_GITHUB']; // these keys are ignored
 const keysToIgnore = new Set(keysToIgnoreFromTemplate);
-
-const envSpecifierType = new String(process.argv[2] || 'test').toUpperCase(); // Also use this to find variables. Always lowercase
-const envSpecifierTypeLower = envSpecifierType.toLowerCase();
-const envSpecifierSeparator = '_';
 
 Object.keys(template)
   .filter((x) => !keysToIgnore.has(x))
@@ -40,7 +53,7 @@ function copyToEnvFromProcessEnv(key, defaultValue = '') {
   const envSpecificKey = `${key}${envSpecifierSeparator}${envSpecifierType}`; // MY_ENV_VARIABLE_TEST
   const envSpecificKeyLower = `${key}${envSpecifierSeparator}${envSpecifierTypeLower}`; // MY_ENV_VARIABLE_test
 
-  env[key] = process.env[envSpecificKey] || process.env[envSpecificKeyLower] || process.env[key] || defaultValue;
+  env[key] = process.env[envSpecificKey] || process.env[envSpecificKeyLower] || process.env[key] || specifierTemplateOverrides[key] || defaultValue;
 }
 
 function initWithProcessEnv(defaultValue, keysSource = env) {
@@ -48,23 +61,31 @@ function initWithProcessEnv(defaultValue, keysSource = env) {
 }
 
 function assertHasNoPlaceholderValues(defaultValue, ignoreKeys = new Set()) {
+  const keysWithPlaceholderValues = [];
+
   Object.keys(env)
     .filter((x) => !ignoreKeys.has(x))
     .forEach((key) => {
       const value = env[key];
 
       if (value === defaultValue) {
-        throw new Error(`The environment varible ${key} had a placeholder value. Ensure it exists in the environment.`);
+        keysWithPlaceholderValues.push(key);
       }
     });
+
+  // throw error with all failed keys.
+  if (keysWithPlaceholderValues.length) {
+    throw new Error(`The ${keysWithPlaceholderValues.length} environment variable(s) ${keysWithPlaceholderValues.map((x) => `"${x}"`).join(', ')} each had a placeholder value set. Ensure each exists in the environment.`);
+  }
 }
+
 // ======================================
 // Configure Here using JS
 // ======================================
 const defaultPlaceholderValue = 'placeholder'; // This is the default value to use if an environment variable that is requested is not defined.
 initWithProcessEnv(defaultPlaceholderValue); // Init with process.env, copying values from process.env onto the existing keys of our env variable
 
-// Check there are no placeholder values remaining
+// Check there are no placeholder values remaining when targeting either staging or prod.
 if (envSpecifierTypeLower === 'staging' || envSpecifierTypeLower === 'prod') {
   assertHasNoPlaceholderValues(defaultPlaceholderValue);
 }
