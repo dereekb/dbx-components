6dbac5ab24cf652f30139a5cd1456aba664d4c26
ci <ci@dereekb.com>
2024-01-12 14:12:39 +0000

    release($workspace): v10.0.3 release
    build: added additional logging to setup-project.sh
    
    build: added lint-fix step to exporting

--- lint-fix-all.sh
+++ lint-fix-all.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# Performs linting on all projects. This process also updates the peer dependencies of all nested package.json files.
 npx nx run-many --exclude=workspace,util-test,firebase-test,firebase-server-test --target=lint --fix
