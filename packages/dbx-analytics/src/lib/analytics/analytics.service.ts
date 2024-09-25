18ea5d10cfaa3f96c19a556d7dd7fdf93329e5c2
Derek Burgman <derek@dereekb.com>
2024-09-25 17:55:49 -0500

    build: lint fix

--- packages/dbx-analytics/src/lib/analytics/analytics.service.ts
+++ packages/dbx-analytics/src/lib/analytics/analytics.service.ts
@@ -1,6 +1,6 @@
-import { Observable, Subject, BehaviorSubject, of, Subscription, first, shareReplay, switchMap, distinctUntilChanged, map, delay } from 'rxjs';
+import { Observable, Subject, BehaviorSubject, of, Subscription, first, shareReplay, switchMap, distinctUntilChanged } from 'rxjs';
 import { Inject, Injectable, Optional } from '@angular/core';
-import { SubscriptionObject, filterMaybe, tapLog } from '@dereekb/rxjs';
+import { SubscriptionObject, filterMaybe } from '@dereekb/rxjs';
 import { DbxAnalyticsEvent, DbxAnalyticsEventData, DbxAnalyticsEventName, DbxAnalyticsUser, NewUserAnalyticsEventData, DbxUserAnalyticsEvent } from './analytics';
 import { DbxAnalyticsStreamEvent, DbxAnalyticsStreamEventType } from './analytics.stream';
 import { Maybe, Destroyable, safeCompareEquality } from '@dereekb/util';
