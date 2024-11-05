9c1bd7e516dc7d8b3f0d6fb19b93aeba72098f9c
Derek Burgman <derek@dereekb.com>
2024-11-04 21:01:44 -0600

    refactor: wrapper demo fix

--- packages/dbx-form/src/lib/formly/field/wrapper/wrapper.key.ts
+++ packages/dbx-form/src/lib/formly/field/wrapper/wrapper.key.ts
@@ -1,5 +1,4 @@
 export const AUTO_TOUCH_WRAPPER_KEY = 'autotouch';
-export const EXPANDABLE_WRAPPER_KEY = 'expandable';
 export const TOGGLE_WRAPPER_KEY = 'toggle';
 export const SECTION_WRAPPER_KEY = 'section';
 export const SUBSECTION_WRAPPER_KEY = 'subsection';
@@ -7,3 +6,6 @@ export const INFO_WRAPPER_KEY = 'info';
 export const FLEX_WRAPPER_KEY = 'flex';
 export const STYLE_WRAPPER_KEY = 'style';
 export const WORKING_WRAPPER_KEY = 'working';
+
+// COMPAT: todo: rename to EXPAND_WRAPPER_KEY and update all references in the next update
+export const EXPANDABLE_WRAPPER_KEY = 'expandable';
