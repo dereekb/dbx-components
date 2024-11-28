1f88a1803eceecc77deec656c33329a7d0336d6e
Derek Burgman <derek@dereekb.com>
2024-11-28 13:53:11 -0600

    refactor: added ZohoDateTimeString

--- packages/zoho/src/lib/zoho.type.ts
+++ packages/zoho/src/lib/zoho.type.ts
@@ -1,4 +1,5 @@
-import { WebsitePath } from '@dereekb/util';
+import { Maybe, MaybeNot, WebsitePath } from '@dereekb/util';
+import { isDataView } from 'util/types';
 
 export interface ZohoModel {}
 
@@ -19,4 +20,33 @@ export interface ZohoGetApiV1Result<T> {
      */
     readonly url: WebsitePath;
   };
+}
+
+/**
+ * Similar to the ISO 8601 date-time format, but with milliseconds removed.
+ *
+ * yyyy-MM-ddTHH:mm:ss±HH:mm
+ *
+ * Examples:
+ * 2019-05-02T11:17:33Z
+ * 2019-05-02T11:17:33+00:00
+ */
+export type ZohoDateTimeString = string;
+
+export function safeZohoDateTimeString(date: Date): ZohoDateTimeString;
+export function safeZohoDateTimeString(date: MaybeNot): MaybeNot;
+export function safeZohoDateTimeString(date: Maybe<Date>): Maybe<ZohoDateTimeString>;
+export function safeZohoDateTimeString(date: Maybe<Date>): Maybe<ZohoDateTimeString> {
+  return date != null ? zohoDateTimeString(date) : date;
+}
+
+/**
+ * Converts the input date to a Zoho date.
+ *
+ * @param date
+ * @returns
+ */
+export function zohoDateTimeString(date: Date): ZohoDateTimeString {
+  const isoDate = date.toISOString();
+  return isoDate.substring(0, isoDate.length - 5) + 'Z';
 }
