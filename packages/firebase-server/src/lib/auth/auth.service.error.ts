adbbd9f020bc9331381ae8db36b857dc6715a102
Derek Burgman <derek@dereekb.com>
2025-04-04 09:58:57 -0500

    refactor: updated FirebaseServerAuthNewUserSendSetupDetailsConfig
    - added additional configuration to sendSetupContent()

---
+++ packages/firebase-server/src/lib/auth/auth.service.error.ts
@@ -0,0 +1,32 @@
+import { Maybe } from '@dereekb/util';
+import { BaseError } from 'make-error';
+
+/**
+ * Thrown by sendSetupDetails() if the user has no setup configuration available, meaning they probably already have accepted their invite or is in an invalid state.
+ */
+export class FirebaseServerAuthNewUserSendSetupDetailsNoSetupConfigError extends BaseError {
+  constructor() {
+    super(`This user has no setup configuration available.`);
+  }
+}
+
+/**
+ * Thrown by sendSetupDetails() if the user was recently sent details.
+ */
+export class FirebaseServerAuthNewUserSendSetupDetailsThrottleError extends BaseError {
+  readonly lastSentAt: Date;
+
+  constructor(lastSentAt: Date) {
+    super(`This user was recently sent details. Try again later.`);
+    this.lastSentAt = lastSentAt;
+  }
+}
+
+/**
+ * Thrown by sendSetupDetails() if the user was recently sent details.
+ */
+export class FirebaseServerAuthNewUserSendSetupDetailsSendOnceError extends BaseError {
+  constructor() {
+    super(`The user has been sent details before and the sendSetupDetailsOnce configuration was true.`);
+  }
+}
--- packages/firebase-server/src/lib/auth/index.ts
+++ packages/firebase-server/src/lib/auth/index.ts
@@ -1,3 +1,4 @@
 export * from './auth.service';
+export * from './auth.service.error';
 export * from './auth.context';
 export * from './auth.util';
