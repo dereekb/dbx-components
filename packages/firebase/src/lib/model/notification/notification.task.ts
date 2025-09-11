78c8a37bffe8e76511ddc54320c7de1b3aee71b7
Derek Burgman <derek@dereekb.com>
2025-09-10 23:01:53 -0500

    refactor: updated NotificationTaskServiceHandleNotificationTaskResult
    - added "removeFromCompletedCheckpoints"  to NotificationTaskServiceHandleNotificationTaskResult to allow resetting or moving backwards in the notification task.
    - added notificationTaskDelayRetry

--- packages/firebase/src/lib/model/notification/notification.task.ts
+++ packages/firebase/src/lib/model/notification/notification.task.ts
@@ -47,9 +47,22 @@ export function delayCompletion(): NotificationTaskServiceTaskHandlerCompletionT
 }
 
 /**
+ * Convenience function for returning a NotificationTaskServiceHandleNotificationTaskResult that says the task should be retried after the specified delay.
+ *
+ * This does not affect the failure/retry count for a notification task.
+ */
+export function notificationTaskDelayRetry<D extends NotificationItemMetadata = {}>(delayUntil: Date | Milliseconds, updateMetadata?: Maybe<Partial<D>>): NotificationTaskServiceHandleNotificationTaskResult<D> {
+  return {
+    completion: delayCompletion(),
+    delayUntil,
+    updateMetadata
+  };
+}
+
+/**
  * Convenience function for returning a NotificationTaskServiceHandleNotificationTaskResult that says the task was partially completed, and to process the next part in the future.
  */
-export function notificationTaskPartiallyComplete<D extends NotificationItemMetadata = {}>(completedParts: ArrayOrValue<NotificationTaskCheckpointString>, updateMetadata?: Partial<D>): NotificationTaskServiceHandleNotificationTaskResult<D> {
+export function notificationTaskPartiallyComplete<D extends NotificationItemMetadata = {}>(completedParts: ArrayOrValue<NotificationTaskCheckpointString>, updateMetadata?: Maybe<Partial<D>>): NotificationTaskServiceHandleNotificationTaskResult<D> {
   return {
     completion: completedParts,
     updateMetadata
@@ -59,7 +72,7 @@ export function notificationTaskPartiallyComplete<D extends NotificationItemMeta
 /**
  * Convenience function for returning a NotificationTaskServiceHandleNotificationTaskResult that says the task was completed successfully.
  */
-export function notificationTaskComplete<D extends NotificationItemMetadata = {}>(updateMetadata?: Partial<D>): NotificationTaskServiceHandleNotificationTaskResult<D> {
+export function notificationTaskComplete<D extends NotificationItemMetadata = {}>(updateMetadata?: Maybe<Partial<D>>): NotificationTaskServiceHandleNotificationTaskResult<D> {
   return {
     completion: true,
     updateMetadata
@@ -69,12 +82,18 @@ export function notificationTaskComplete<D extends NotificationItemMetadata = {}
 /**
  * Convenience function for returning a NotificationTaskServiceHandleNotificationTaskResult that says the task failed.
  */
-export function notificationTaskFailed<D extends NotificationItemMetadata = {}>(updateMetadata?: Partial<D>): NotificationTaskServiceHandleNotificationTaskResult<D> {
+export function notificationTaskFailed<D extends NotificationItemMetadata = {}>(updateMetadata?: Maybe<Partial<D>>, removeFromCompletedCheckpoints?: Maybe<ArrayOrValue<NotificationTaskCheckpointString>>): NotificationTaskServiceHandleNotificationTaskResult<D> {
   return {
     completion: false,
-    updateMetadata
+    updateMetadata,
+    removeFromCompletedCheckpoints
   };
 }
+
+/**
+ * One or more NotificationTaskCheckpointString values that are considered complete.
+ */
+export type NotificationTaskServiceTaskHandlerCompletionTypeCheckpoint = ArrayOrValue<NotificationTaskCheckpointString>;
 
 /**
  * Result of a NotificationTaskServiceTaskHandler.handleNotificationTask() call.
@@ -83,7 +102,7 @@ export function notificationTaskFailed<D extends NotificationItemMetadata = {}>(
  * false: The task was not completed successfully and should be retried again in the future. Note there are a maximum number of retry attempts before the task is deleted. Use delayCompletion() to avoid increasing the attempt count.
  * NotificationTaskCheckpointString(s): The task has successfully completed this/these particular checkpoint(s) but is not complete and should be continued again in the future. Return an empty array to signal that the task did not fail but has not reached the next checkpoint.
  */
-export type NotificationTaskServiceTaskHandlerCompletionType = true | false | ArrayOrValue<NotificationTaskCheckpointString>;
+export type NotificationTaskServiceTaskHandlerCompletionType = true | false | NotificationTaskServiceTaskHandlerCompletionTypeCheckpoint;
 
 // MARK: Server
 /**
@@ -95,11 +114,25 @@ export interface NotificationTaskServiceHandleNotificationTaskResult<D extends N
    */
   readonly completion: NotificationTaskServiceTaskHandlerCompletionType;
   /**
+   * If true, clears all completed checkpoints.
+   */
+  readonly removeAllCompletedCheckpoints?: boolean;
+  /**
+   * Removes the specified completions from the notification task.
+   *
+   * This is applied before the returned completion result, so it only removes from the current completion array.
+   *
+   * Only used if the completion type is NotificationTaskServiceTaskHandlerCompletionTypeCheckpoint.
+   *
+   * Ignored if removeAllCompletedCheckpoints is true.
+   */
+  readonly removeFromCompletedCheckpoints?: Maybe<ArrayOrValue<NotificationTaskCheckpointString>>;
+  /**
    * Updates the metadata for the notification item if the task is successful but not yet marked done.
    *
    * Does not update the metadata if the completion type is true or false.
    */
-  readonly updateMetadata?: Partial<D>;
+  readonly updateMetadata?: Maybe<Partial<D>>;
   /**
    * Delays the next run of the task by the specified amount of time or until the given date.
    */
