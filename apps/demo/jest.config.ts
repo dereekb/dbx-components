670e0a000011c37e175da1f5e28c410bef3b684c
Derek Burgman <derek@dereekb.com>
2026-02-25 13:39:04 -0600

    refactor: update demo to use vitest

--- apps/demo/jest.config.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-(global as any).appTestType = 'angular';
-
-module.exports = {
-  displayName: 'demo',
-  coverageDirectory: '../../coverage/apps/demo',
-  preset: '../../jest.preset.ts'
-};
