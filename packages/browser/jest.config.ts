d4bca78e278308b337cb61e9778a9abd6f8ccbde
Derek Burgman <derek@dereekb.com>
2026-02-25 13:43:57 -0600

    refactor: updated remaining projects to vitest

--- packages/browser/jest.config.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-module.exports = {
-  displayName: 'browser',
-  coverageDirectory: '../../coverage/packages/browser',
-  preset: '../../jest.preset.ts'
-};
--- packages/util/fetch/jest.config.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-(global as any).testFolderRootPath = '<rootDir>/../../..';
-(global as any).appTestType = 'node';
-
-module.exports = {
-  displayName: 'util-fetch',
-  preset: '../../../jest.preset.ts',
-  coverageDirectory: '../../../coverage/packages/util/fetch'
-};
