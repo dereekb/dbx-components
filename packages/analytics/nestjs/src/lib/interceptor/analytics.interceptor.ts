46d9ad1959b55508b7766cc097672883ee6b0c8e
Derek Burgman <derek@dereekb.com>
2026-04-02 14:39:15 -0500

    build: lint fix

--- packages/analytics/nestjs/src/lib/interceptor/analytics.interceptor.ts
+++ packages/analytics/nestjs/src/lib/interceptor/analytics.interceptor.ts
@@ -1,6 +1,6 @@
 import { type Maybe } from '@dereekb/util';
 import { type AnalyticsEventName } from '@dereekb/analytics';
-import { type CallHandler, type ExecutionContext, Injectable, type NestInterceptor, SetMetadata } from '@nestjs/common';
+import { type CallHandler, type ExecutionContext, Injectable, type NestInterceptor, SetMetadata, Inject } from '@nestjs/common';
 import { type Reflector } from '@nestjs/core';
 import { type EventEmitter2 } from '@nestjs/event-emitter';
 import { type Observable } from 'rxjs';
@@ -64,8 +64,8 @@ export const EmitAnalyticsEvent = <T>(config: AnalyticsEventInterceptorConfig<T>
 @Injectable()
 export class AnalyticsEventInterceptor<T = any> implements NestInterceptor {
   constructor(
-    readonly reflector: Reflector,
-    readonly eventEmitter: EventEmitter2
+    @Inject(Reflector) readonly reflector: Reflector,
+    @Inject(EventEmitter2) readonly eventEmitter: EventEmitter2
   ) {}
 
   /**
