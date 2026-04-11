783b8eac5ed31b065586e2b611a35bc1e08eae04
Derek Burgman <derek@dereekb.com>
2026-04-11 11:34:15 -0500

    refactor: use specific field def types in forge registry
    - replaced BaseValueField<unknown, unknown> with concrete types
    - uses proper DbxForge*FieldDef types for each registered field
    - generic fields use any for type params in registry

--- packages/dbx-form/src/lib/forge/forge.registry.ts
+++ packages/dbx-form/src/lib/forge/forge.registry.ts
@@ -7,37 +7,62 @@
  *
  * @see https://www.ng-forge.com/dynamic-forms/custom/building-an-adapter
  */
-import type { BaseValueField } from '@ng-forge/dynamic-forms';
-import type { DbxForgeSliderFieldProps } from './field/value/number/slider.field.component';
-import type { DbxForgeSectionFieldProps } from './field/wrapper/section/section.field';
-import type { DbxForgeFormFieldWrapperProps } from './field/wrapper/formfield/formfield.field';
+
+// Value field types
+import type { DbxForgePhoneFieldDef } from './field/value/phone/phone.field';
+import type { DbxForgeDateTimeFieldDef, DbxForgeFixedDateRangeFieldDef } from './field/value/date/datetime.field';
+import type { DbxForgeTimeDurationFieldDef } from './field/value/duration/duration.field';
+import type { DbxForgeSliderFieldDef } from './field/value/number/slider.field.component';
+import type { DbxForgeArrayFieldDef } from './field/value/array/array.field';
+
+// Selection field types
+import type { DbxForgeSearchableTextFieldDef, DbxForgeSearchableChipFieldDef } from './field/selection/searchable/searchable.field.directive';
+import type { DbxForgePickableChipFieldDef, DbxForgePickableListFieldDef } from './field/selection/pickable/pickable.field.directive';
+import type { DbxForgeListSelectionFieldDef } from './field/selection/list/list.field.component';
+import type { DbxForgeValueSelectionFieldDef } from './field/selection/selection.field.component';
+import type { DbxForgeSourceSelectFieldDef } from './field/selection/sourceselect/sourceselect.field.component';
+
+// Other field types
+import type { DbxForgeTextEditorFieldDef } from './field/texteditor/texteditor.field.component';
+import type { DbxForgeComponentFieldDef } from './field/component/component.field.component';
+
+// Wrapper field types
+import type { DbxForgeFormFieldWrapperFieldDef } from './field/wrapper/formfield/formfield.field';
+import type { DbxForgeSectionFieldDef } from './field/wrapper/section/section.field';
+import type { DbxForgeExpandFieldDef } from './field/wrapper/expand/expand.field';
+import type { DbxForgeInfoButtonFieldDef } from './field/wrapper/info/info.field';
+import type { DbxForgeInfoWrapperFieldDef } from './field/wrapper/info/info.wrapper.field';
+import type { DbxForgeStyleFieldDef } from './field/wrapper/style/style.field';
+import type { DbxForgeWorkingFieldDef } from './field/wrapper/working/working.field';
+import type { DbxForgeWorkingWrapperFieldDef } from './field/wrapper/working/working.wrapper.field';
 
 declare module '@ng-forge/dynamic-forms' {
   interface FieldRegistryLeaves {
     // Value fields
-    phone: BaseValueField<unknown, unknown>;
-    datetime: BaseValueField<unknown, unknown>;
-    fixeddaterange: BaseValueField<unknown, unknown>;
-    timeduration: BaseValueField<unknown, unknown>;
-    'dbx-slider': BaseValueField<DbxForgeSliderFieldProps, number>;
-    'dbx-searchable-text': BaseValueField<unknown, unknown>;
-    'dbx-searchable-chip': BaseValueField<unknown, unknown>;
-    'dbx-pickable-chip': BaseValueField<unknown, unknown>;
-    'dbx-pickable-list': BaseValueField<unknown, unknown>;
-    'dbx-list-selection': BaseValueField<unknown, unknown>;
-    'dbx-source-select': BaseValueField<unknown, unknown>;
-    'dbx-text-editor': BaseValueField<unknown, unknown>;
-    'dbx-component': BaseValueField<unknown, unknown>;
+    phone: DbxForgePhoneFieldDef;
+    datetime: DbxForgeDateTimeFieldDef;
+    fixeddaterange: DbxForgeFixedDateRangeFieldDef;
+    timeduration: DbxForgeTimeDurationFieldDef;
+    'dbx-slider': DbxForgeSliderFieldDef;
+    'dbx-searchable-text': DbxForgeSearchableTextFieldDef<any, any, any>;
+    'dbx-searchable-chip': DbxForgeSearchableChipFieldDef<any, any, any>;
+    'dbx-pickable-chip': DbxForgePickableChipFieldDef<any, any, any>;
+    'dbx-pickable-list': DbxForgePickableListFieldDef<any, any, any>;
+    'dbx-list-selection': DbxForgeListSelectionFieldDef<any, any, any>;
+    'dbx-value-selection': DbxForgeValueSelectionFieldDef<any>;
+    'dbx-source-select': DbxForgeSourceSelectFieldDef<any, any>;
+    'dbx-text-editor': DbxForgeTextEditorFieldDef;
+    'dbx-component': DbxForgeComponentFieldDef<any>;
 
     // Wrapper fields (registered as leaves since they use valueFieldMapper)
-    'dbx-forge-form-field': BaseValueField<DbxForgeFormFieldWrapperProps, Record<string, unknown>>;
-    'dbx-forge-section': BaseValueField<DbxForgeSectionFieldProps, Record<string, unknown>>;
-    'dbx-forge-expand': BaseValueField<unknown, unknown>;
-    'dbx-forge-info-button': BaseValueField<unknown, unknown>;
-    'dbx-forge-info': BaseValueField<unknown, unknown>;
-    'dbx-forge-style': BaseValueField<unknown, unknown>;
-    'dbx-forge-working': BaseValueField<unknown, unknown>;
-    'dbx-forge-working-wrapper': BaseValueField<unknown, unknown>;
-    'dbx-forge-array': BaseValueField<unknown, unknown>;
+    'dbx-forge-form-field': DbxForgeFormFieldWrapperFieldDef;
+    'dbx-forge-section': DbxForgeSectionFieldDef;
+    'dbx-forge-expand': DbxForgeExpandFieldDef;
+    'dbx-forge-info-button': DbxForgeInfoButtonFieldDef;
+    'dbx-forge-info': DbxForgeInfoWrapperFieldDef;
+    'dbx-forge-style': DbxForgeStyleFieldDef;
+    'dbx-forge-working': DbxForgeWorkingFieldDef;
+    'dbx-forge-working-wrapper': DbxForgeWorkingWrapperFieldDef;
+    'dbx-forge-array': DbxForgeArrayFieldDef<any>;
   }
 }
