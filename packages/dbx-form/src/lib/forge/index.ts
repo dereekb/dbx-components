80516abac47c971adc5904622984bec18ee7dca5
Derek Burgman <derek@dereekb.com>
2026-05-04 22:20:29 -0500

    refactor: update ng-forge version

--- packages/dbx-form/src/lib/forge/index.ts
+++ packages/dbx-form/src/lib/forge/index.ts
@@ -1,7 +1,7 @@
 export * from './field';
 export * from './form';
 export * from './forge.providers';
-import './forge.registry';
+export * from './forge.registry';
 export * from './preset';
 export * from './template';
 export * from './validation';
